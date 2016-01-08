4d5f477df857d72349cb703ca221c13ab630852c
Pafcholini <nadyaivanova14@gmail.com>
2016-01-08 12:34:25 +0100

    Drop POK1 Source Code
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,7 @@ export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
 mkdir output
 
-make -C $(pwd) O=output VARIANT_DEFCONFIG=apq8084_sec_trlte_tmo_defconfig apq8084_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output VARIANT_DEFCONFIG=apq8084_sec_trlte_eur_defconfig apq8084_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
